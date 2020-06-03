99ef46ddfdce54f01265b5cef01f54291336de25
jpouabou <jm.pouabou@gmail.com>
2020-06-02 19:51:35 -0700

    Added people-list-component to the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import GetInvolved from '../components/ui/getInvolved/GetInvolved';
-
+import PeopleList from '../components/ui/PeopleList/PeopleList'
 const Home = () => (
   <div className="App">
     <GetInvolved />
+    <PeopleList />
   </div>
 );
 
